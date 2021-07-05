f47dd20dc68ed6cd5e96f7129e99ca200f722bae
Ahmed Adel <ahmedelbehairy1996@gmail.com>
2021-07-05 18:42:44 +0200

    edit according to ahmed essam's review

--- C crash course tasks/S_CP_01 X-O Game/main.c
+++ C crash course tasks/S_CP_01 X-O Game/main.c
@@ -13,8 +13,30 @@ int main()
     UnitTestingDriver();
     UnitTestingFrameWork();
     #else
-    start();
-    printf("\npress enter to continue");
+    while(1)
+    {
+        system("cls");
+        int x =0;
+        start();
+        printf("if you want to play again press 1 or press 2 to terminate\n");
+        x=getch()-48;
+        if(x==1)
+        {
+            continue;
+        }
+        else if(x==2)
+        {
+            break;
+        }
+        else
+        {
+            printf("wrong entry program will terminate");
+            break;
+        }
+
+    }
+
+    printf("\npress enter to terminate");
     getch();
     system("cls");
     #endif // Unit_Testing

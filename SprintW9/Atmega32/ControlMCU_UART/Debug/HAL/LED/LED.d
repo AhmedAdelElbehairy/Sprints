f24f2317c2e41920be23d2727c1c17221feb1c37
Ahmed Adel <ahmedelbehairy1996@gmail.com>
2021-07-25 19:53:42 +0200

    Control MCU Using UART

---
+++ SprintW9/Atmega32/ControlMCU_UART/Debug/HAL/LED/LED.d
@@ -0,0 +1,15 @@
+HAL/LED/LED.o HAL/LED/LED.o: ../HAL/LED/LED.c \
+  ../HAL/LED/../../INFRASTRUCTURE/LIB/STD_TYPES.h \
+  ../HAL/LED/../../INFRASTRUCTURE/LIB/Bit\ Operations.h \
+  ../HAL/LED/../../MCAL/DIO/DIO_DRIVER_TypeDef.h \
+  ../HAL/LED/../../MCAL/DIO/DIO_DRIVER.h ../HAL/LED/LED.h
+
+../HAL/LED/../../INFRASTRUCTURE/LIB/STD_TYPES.h:
+
+../HAL/LED/../../INFRASTRUCTURE/LIB/Bit\ Operations.h:
+
+../HAL/LED/../../MCAL/DIO/DIO_DRIVER_TypeDef.h:
+
+../HAL/LED/../../MCAL/DIO/DIO_DRIVER.h:
+
+../HAL/LED/LED.h:
---
+++ SprintW9/Atmega32/ControlMCU_UART/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 15, 2021
+ *      Author: Ahmed
+ */
+#define F_CPU 8000000UL
+#include "Infrastructure/LIB/STD_TYPES.h"
+#include "Infrastructure/LIB/Bit Operations.h"
+#include "MCAL/DIO/DIO_DRIVER_TypeDef.h"
+#include "MCAL/DIO/DIO_DRIVER.h"
+#include "MCAL/UART/UART.h"
+#include "APP/APP.h"
+int main() {
+	APP_Init();
+
+	while (1) {
+		APP_Start();
+	}
+}

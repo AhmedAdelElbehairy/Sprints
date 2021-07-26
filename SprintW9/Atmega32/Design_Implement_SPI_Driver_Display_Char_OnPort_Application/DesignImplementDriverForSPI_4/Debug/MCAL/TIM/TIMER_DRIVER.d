d0f8d273b107c37cd7439c564d8465dcdbc0032a
Ahmed Adel <ahmedelbehairy1996@gmail.com>
2021-07-26 16:20:20 +0200

    SPI Driver Display Character On Port task

---
+++ SprintW9/Atmega32/Design_Implement_SPI_Driver_Display_Char_OnPort_Application/DesignImplementDriverForSPI_4/Debug/MCAL/TIM/TIMER_DRIVER.d
@@ -0,0 +1,21 @@
+MCAL/TIM/TIMER_DRIVER.o MCAL/TIM/TIMER_DRIVER.o:  \
+ ../MCAL/TIM/TIMER_DRIVER.c \
+  ../MCAL/TIM/../../Infrastructure/LIB/STD_TYPES.h \
+  ../MCAL/TIM/../../Infrastructure/LIB/Bit\ Operations.h \
+  ../MCAL/TIM/../DIO/DIO_DRIVER_TypeDef.h ../MCAL/TIM/../DIO/DIO_DRIVER.h \
+  ../MCAL/TIM/TIMER_DRIVER_typedefs.h ../MCAL/TIM/TIMER_DRIVER_PRIV.h \
+  ../MCAL/TIM/TIMER_DRIVER.h
+
+../MCAL/TIM/../../Infrastructure/LIB/STD_TYPES.h:
+
+../MCAL/TIM/../../Infrastructure/LIB/Bit\ Operations.h:
+
+../MCAL/TIM/../DIO/DIO_DRIVER_TypeDef.h:
+
+../MCAL/TIM/../DIO/DIO_DRIVER.h:
+
+../MCAL/TIM/TIMER_DRIVER_typedefs.h:
+
+../MCAL/TIM/TIMER_DRIVER_PRIV.h:
+
+../MCAL/TIM/TIMER_DRIVER.h:
